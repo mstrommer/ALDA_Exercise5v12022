61a1cd51639bc5a5eff49c00e6db7b21988647b6
Michael Strommer <69633126+mstrommer@users.noreply.github.com>
2022-06-03 15:36:20 +0200

    Initial Structure

---
+++ hashtable.cpp
@@ -0,0 +1,8 @@
+//
+//  hashtable.cpp
+//  ALDA_Exercise5v12022
+//
+//  Created by Michael Strommer on 03.06.22.
+//
+
+#include "hashtable.hpp"
---
+++ hashtable.hpp
@@ -0,0 +1,13 @@
+//
+//  hashtable.hpp
+//  ALDA_Exercise5v12022
+//
+//  Created by Michael Strommer on 03.06.22.
+//
+
+#ifndef hashtable_hpp
+#define hashtable_hpp
+
+#include <stdio.h>
+
+#endif /* hashtable_hpp */
